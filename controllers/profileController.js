0c3677eef44dfd80ec23d013954ab89b2356a9f7
Yasen Ivanov <yasen.denchev@gmail.com>
2017-05-07 20:41:30 +0300

    added option to change profile information

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,28 +2,83 @@ import {templates} from '../utils/templates.js';
 
 export function profileController() {
 
-    var user = firebase.auth().currentUser
+    let user = firebase.auth().currentUser;
     templates.get('profile')
         .then((res) => {
 
-            user.updateProfile({
-                displayName: "Vanko",
-                photoURL: ""
-            });
             let hbTemplate = Handlebars.compile(res);
             let template = hbTemplate(user);
 
             $('#content').html(template);
-            let profilePic = document.getElementById('profilePic');
+
+            let changePicBtn = document.getElementById('changePicBtn');
+            changePicBtn.addEventListener('click', showPictureChangeContainer);
+
+            let savePicBtn = document.getElementById('savePicBtn');
+            savePicBtn.addEventListener('click', function () {
+                submitPictureUrl(user);
+            });
+
+            let cancelPicBtn = document.getElementById('cancelPicBtn');
+            let changePicContainer = document.getElementById('changePicContainer');
+            cancelPicBtn.addEventListener('click', function () {
+                hide(changePicContainer);
+            });
+
+            let editEmailBtn = document.getElementById('editEmailBtn');
+            let changeEmailContainer = document.getElementById('changeEmailContainer');
+            editEmailBtn.addEventListener('click', function() {
+                show(changeEmailContainer);
+            });
+            
+            let saveEmailBtn = document.getElementById('')
         });
-    var markup = document.documentElement.innerHTML;
-    console.log(markup);
-    var changePicBtn = document.getElementById("changePic");
-    changePicBtn.addEventListener('click', function () {
-        document.getElementById('changePicContainer').style.display('');
-    })
+}
+function show(element) {
+    element.style.display = "";
 }
 
-function addProfilePicFunction() {
+function hide(element) {
+    element.style.display = "none";
+}
 
+function showEmailChangeContainer() {
+    let changeEmailContainer = document.getElementById('changeEmailContainer');
+    changeEmailContainer.style.display = "block";
+}
+
+function showPictureChangeContainer() {
+    let changePicContainer = document.getElementById('changePicContainer');
+    changePicContainer.style.display = "block";
+}
+
+function hidePictureChangeContainer(){
+    let changePicContainer = document.getElementById('changePicContainer');
+    changePicContainer.style.display = "none";
+}
+
+function submitPictureUrl(user) {
+    const changePicContainer = document.getElementById('changePicContainer');
+    const pattern = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+
+    let urlTextBox = document.getElementById('urlTextBox');
+    let url = urlTextBox.value;
+
+
+    if (pattern.test(url) == true) {
+        user.updateProfile({
+            photoURL: url
+        });
+
+        let profilePic = document.getElementById('profilePicture');
+        profilePic.src = url;
+    }
+
+    else {
+        alert("Invalid URL!");
+        return;
+    }
+
+    urlTextBox.value = ""
+    hide(changePicContainer);
 }
